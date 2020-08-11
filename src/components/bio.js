21d13a894ac13628067346dc612e80c5e5d94ed3
c0dr <simon.schraeder@yahoo.de>
2020-08-11 12:07:36 +0000

    fix bio image

--- src/components/bio.js
+++ src/components/bio.js
@@ -14,12 +14,12 @@ import { rhythm } from "../utils/typography"
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
-      avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      avatar: file(absolutePath: { regex: "/payment.jpg/" }) {
         childImageSharp {
-          fixed(width: 50, height: 50) {
-            ...GatsbyImageSharpFixed
-          }
-        }
+            fluid(maxWidth: 700, maxHeight: 300) {
+            ...GatsbyImageSharpFluid
+            }
+        }        
       }
       site {
         siteMetadata {
@@ -44,21 +44,17 @@ const Bio = () => {
       }}
     >
       <Image
-        fixed={data.avatar.childImageSharp.fixed}
+        fluid={data.avatar.childImageSharp.fluid}
         alt={author.name}
         style={{
           marginRight: rhythm(1 / 2),
           marginBottom: 0,
-          minWidth: 50,
-          borderRadius: `100%`
-        }}
-        imgStyle={{
-          borderRadius: `50%`
+          minWidth: 150,
         }}
       />
       <p>
         Bei <a href="https://cardonly.de">CardOnly.de</a> dreht sich alles um Kartenzahlung und Banking - in
-        unserem <a href="https://cardonly.de">Vergleich </a>Vergleich findet ihr eure beste Karte, im Blog alle aktuellen
+        unserem <a href="https://cardonly.de">Vergleich </a> findet ihr eure beste Karte, im Blog alle aktuellen
         News.
         {` `}
         <a href={`https://twitter.com/${social.twitter}`}>Kurzmeldungen auch bei Twitter</a>
